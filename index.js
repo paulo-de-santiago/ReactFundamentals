3fadafb6c7252fb1d87aea812d52f9f88c8b23e7
Paulo Santiago <pauloecsantiago@gmail.com>
2023-04-17 11:42:48 +0100

    Prop children is an object but multiple children transforms into an array

--- index.js
+++ index.js
@@ -57,3 +57,17 @@ React.createElement(Button, {children: element})
 React.createElement(Button, {}, element)
 //3
 React.createElement(Button, {children: element})
+
+
+/* Children has the type of object but with multiple children it turns it into an Array */
+
+<Button>
+  {element} {element2}
+
+  {stuff.map(()=> {element3and4})}
+
+</Button>
+//1) Multiple children under the hood
+React.createElement(Button, {}, element, element2, [element3, element4])
+//2) Multiple children under the hood
+React.createElement(Button, {children: [[element1, element2, [element3, element4]]]})
